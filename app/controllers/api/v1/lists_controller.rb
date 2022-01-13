ca4767351d65a59223aafc8860ceab507a4fdd77
msalerno93 <msalerno1993@gmail.com>
2022-01-13 18:35:36 -0500

    Create route and Index route

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::ListsController < ApplicationController
 
     def index
-        @lists = List.all
-        render json: @lists
+        lists = List.all
+        render json: lists
+    end
+
+    def create
+        list = List.new(list_params)
+        if list.save
+            render json: list, status: :accepted
+       else
+           render json: {errors: list.errors.full_messages}, status: :unprocessible_entity
+       end
+
+    end
+
+
+    private
+
+    def list_params
+        params.require(:list).permit(:title, :description, :category_id)
     end
 end
