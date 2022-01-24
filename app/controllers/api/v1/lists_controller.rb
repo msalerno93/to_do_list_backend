f58350cedc4deb5ac26a413a91970ec7968f0ac3
msalerno93 <msalerno1993@gmail.com>
2022-01-23 19:02:37 -0500

    fix create method

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -2,16 +2,13 @@ class Api::V1::ListsController < ApplicationController
 
     def index
         lists = List.all
-        # options = {
-        #     include: [:category]
-        # }
         render json: ListSerializer.new(lists)
     end
 
     def create
         list = List.new(list_params)
         if list.save
-            render json: list, include: [:category], status: :accepted
+            render json: ListSerializer.new(list), status: :accepted
         else
            render json: {errors: list.errors.full_messages}, status: :unprocessible_entity
         end
