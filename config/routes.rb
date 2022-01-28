d3ef20fd75d6b6448e71e64860b73b71b8aa8a20
msalerno93 <msalerno1993@gmail.com>
2022-01-27 21:04:44 -0500

    fixed delete issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :lists, only: [:index, :create]
+      resources :lists, only: [:index, :create, :destroy]
       resources :categories, only: [:index]
     end
   end
